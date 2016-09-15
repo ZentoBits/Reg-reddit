a7ad9c23403bb9891fe78c2597ed9d73c0abe879
Zachary George <zacharygeorge@Zacharys-MacBook-Pro.local>
2016-09-15 16:03:27 -0500

    added post functionality

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict'
 
 const express = require('express');
-
-const { connect } = require('./database')
-
 const app = express()
+const bodyParser = require('body-parser')
+
+// exports
+const { connect } = require('./database')
+const makePost = require('./models/makePost')
 
 const port = process.env.PORT || 3000
 app.set('port', port)
@@ -13,14 +15,23 @@ app.set('view engine', 'pug')
 
 // middlewares
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // routes
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.get('/post', (req, res) => {
-  res.render('post')
+app.get('/makePost', (req, res) => {
+  res.render('makePost')
+})
+
+app.post('/makePost', (req, res) => {
+  console.log('touch my body', req.body);
+  makePost
+  .create(req.body)
+  .then(() => res.redirect('/'))
+  .catch(console.error)
 })
 
 connect()
