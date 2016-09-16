fc95282b32a3a62ebd2be6572f081b8468fd1f0a
Vaughn Walters <vaughnwalters@gmail.com>
2016-09-16 14:33:29 -0500

    fix in database.js file

--- database.js
+++ database.js
@@ -2,11 +2,11 @@
 
 const mongoose = require('mongoose')
 
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/regreddit'
+const MONGOOSE_URL = process.env.MONGOOSE_URL || 'mongodb://localhost:27017/regreddit'
 
 mongoose.Promise = Promise
 
-module.exports.connect = () => mongoose.connect(MONGODB_URL)
+module.exports.connect = () => mongoose.connect(MONGOOSE_URL)
 module.exports.disconnect = () => mongoose.disconnect()
 
 
