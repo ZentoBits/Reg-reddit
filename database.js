8e2332b16ce13f8014eca5f065a94afac810323c
Vaughn Walters <vaughnwalters@gmail.com>
2016-09-16 13:49:50 -0500

    first deploy to heroku

--- database.js
+++ database.js
@@ -2,8 +2,21 @@
 
 const mongoose = require('mongoose')
 
-const MONGOOSE_URL = 'mongodb://localhost:27017/regreddit'
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/regreddit'
 
 mongoose.Promise = Promise
 
-module.exports.connect = () => mongoose.connect(MONGOOSE_URL)
+module.exports.connect = () => mongoose.connect(MONGODB_URL)
+module.exports.disconnect = () => mongoose.disconnect()
+
+
+// 'use strict'
+
+// const mongoose = require('mongoose')
+
+// const MONGOOSE_URL = 'mongodb://localhost:27017/regreddit'
+
+// mongoose.Promise = Promise
+
+// module.exports.connect = () => mongoose.connect(MONGOOSE_URL)
+
